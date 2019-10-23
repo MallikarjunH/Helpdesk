482a786b5fdf741ea17394290e4345e31b8eb1e9
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-10-23 17:06:27 +0530

    HelpdeskPro - Added App Icons

---
+++ FaveoHelpdeskPro_ObjC/FaveoHelpdeskPro_ObjC/Controllers/Login/LoginViewController.m
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.m
+//  FaveoHelpdeskPro_ObjC
+//
+//  Created by mallikarjun on 23/10/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
