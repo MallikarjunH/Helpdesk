1a0a05d987a724c22fb54b196b38818e04dce978
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-10-31 17:34:58 +0530

    Helpdesk - Swift Project
    Helpdesk - Swift Starter Project  Added

---
+++ FaveoHelpdeskPro_Swift/FaveoHelpdeskPro_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaveoHelpdeskPro_Swift
+//
+//  Created by mallikarjun on 31/10/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
