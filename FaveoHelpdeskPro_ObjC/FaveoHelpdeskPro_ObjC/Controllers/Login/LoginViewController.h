757ab754b7b49e7e1b08e0fc89f80ca482b659eb
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-04 15:49:41 +0530

    pod update

--- FaveoHelpdeskPro_ObjC/FaveoHelpdeskPro_ObjC/Controllers/Login/LoginViewController.h
+++ FaveoHelpdeskPro_ObjC/FaveoHelpdeskPro_ObjC/Controllers/Login/LoginViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SVProgressHUD.h"
 //#import "AppCon"
 
 NS_ASSUME_NONNULL_BEGIN
--- FaveoHelpdeskPro_ObjC/FaveoHelpdeskPro_ObjC/Controllers/Login/LoginViewController.m
+++ FaveoHelpdeskPro_ObjC/FaveoHelpdeskPro_ObjC/Controllers/Login/LoginViewController.m
@@ -11,6 +11,7 @@
 #import "Reachability.h"
 #import "MyWebservices.h"
 
+
 @interface LoginViewController () <UITextFieldDelegate>
 {
     Utils *utils;
